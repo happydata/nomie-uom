62a4033c820247ce0dbca373b932c5b122f65a06
Brandon Corbin <brandon@icorbin.com>
2016-09-10 19:29:09 -0400

    Timer display formating 1h 2m 33s

--- nomie-uom.js
+++ nomie-uom.js
@@ -125,10 +125,12 @@ var NomieUOM = function() {
 				 var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
 				 var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-				 if (hours   < 10) {hours   = "0"+hours;}
-				 if (minutes < 10) {minutes = "0"+minutes;}
-				 if (seconds < 10) {seconds = "0"+seconds;}
-				 return hours+':'+minutes+':'+seconds;
+				//  if (hours   < 10) {hours   = "0"+hours;}
+				//  if (minutes < 10) {minutes = "0"+minutes;}
+				//  if (seconds < 10) {seconds = "0"+seconds;}
+
+				 return (!hours) ? minutes+'m '+seconds+'s' : hours+'h '+minutes+'m '+seconds+'s';
+
 			}
 		},
 		sec: {
