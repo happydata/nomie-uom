8a4eac67b31dece39922bd42f8efc132836484d0
Brandon Corbin <brandon@icorbin.com>
2017-07-10 19:04:31 -0400

    Adding TS definition

---
+++ nomie-uom.d.ts
@@ -0,0 +1,9 @@
+declare namespace nomieUOM {
+	function toArray(): Array<any>;
+	function toGroupedArray(): Array<any>;
+	function plural(key:String): String;
+	function singular(key:String): String;
+	function abv(key:String): String;
+	function addCommas(num:Number): String;
+	function displayValue()
+}
